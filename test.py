24ef6f2ba4ccea82bb9e9741118f1c2fcc256104
BP22016 <bp22016@shibaura-it.ac.jp>
2024-11-27 14:09:42 +0900

    コミットしてみた

---
+++ test.py
@@ -0,0 +1,21 @@
+import sys
+import hakosim
+
+def main():
+    
+    if len(sys.argv)!= 2:
+        print(f"Usage:{sys.argv[0]}<config_path>")
+        return 1
+        #sys.argv[0] = "..\config\custom.json"
+    
+    client = hakosim.MultirotorClient(sys.argv[1])
+    client.confirmConnection()
+    client.enableApiControl(True)
+    client.armDisarm(True)
+
+    client.takeoff(3)
+
+    return 0
+
+if __name__=="__main__":
+    sys.exit(main())
